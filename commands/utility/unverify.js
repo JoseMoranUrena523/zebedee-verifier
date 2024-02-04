eedcc4c960dc8f5b1e89bfbe426425f43c18755b
Jose Moran Urena <jose@josemoranurena.tech>
2024-02-04 12:15:13 -0500

    Update unverify.js

--- commands/utility/unverify.js
+++ commands/utility/unverify.js
@@ -8,8 +8,21 @@ module.exports = {
 		.setName('unverify')
 		.setDescription('Unlinks the ZEBEDEE account set to your Discord.'),
 	async execute(interaction) {
-    const member = interaction.member;
-    await db.delete(member.id + "_verify");
-		await interaction.reply('ZEBEDEE account has been removed!');
+		const member = interaction.member;
+    const role = await db.get(interaction.guildId + "_role");
+
+		if (!role) {
+			await interaction.reply('Role has not been set.');
+			return;
+		}
+
+    if (member.roles.cache.some(checkRole => checkRole.name === role.name)) {
+      await member.roles.remove(role.id)
+	    await db.delete(member.id + "_verify");
+		  await interaction.reply('ZEBEDEE account has been removed!');
+      return;
+    }
+
+    await interaction.reply('You are not verified.');
 	},
 };
