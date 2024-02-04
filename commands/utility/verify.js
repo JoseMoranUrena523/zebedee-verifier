9470de94d4a1f074dd56e7dda189ea33cadfd383
Jose Moran Urena <jose@josemoranurena.tech>
2024-02-04 09:07:45 -0500

    Update verify.js

--- commands/utility/verify.js
+++ commands/utility/verify.js
@@ -1,8 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { zbd }  = require("@zbd/node");
-
-const ZBD_API_KEY = '4DoIrzVXOiFrK8YSytRyvgPkbSOFr4kC';
-const ZBD = new zbd(ZBD_API_KEY);
 
 const { QuickDB } = require("quick.db");
 const db = new QuickDB();
@@ -12,15 +8,16 @@ module.exports = {
 		.setName('verify')
 		.setDescription('Verifies ZEBEDEE gamertag and Discord account.'),
 	async execute(interaction) {
-		await interaction.reply('Checking to see if you went through verification process...');
 		const member = interaction.member;
 		const role = await db.get(interaction.guildId + "_role");
 		const isVerified = await db.get(member.id + "_verify");
 
 		if (!role) {
-		  await interaction.editReply("An administrator hasn't configured a role to set when verified.");
+		  await interaction.reply("An administrator hasn't configured a role to set when verified.");
 		  return;
 		}
+
+		await interaction.reply('Checking to see if you went through the verification process...');
 		
 		if (member.roles.cache.some(roleCheck => roleCheck.name === role.name)) {
 		  await interaction.editReply("You are already verified.")
@@ -28,11 +25,11 @@ module.exports = {
 		}
 
 		if (!isVerified) {
-		  await interaction.editReply(`To verify, please visit: https://verifierbot.josemoranurena.tech/login?discord=${member.id}.`);
+		  await interaction.editReply(`Please click [this link](https://verifierbot.josemoranurena.tech/login?discord=${member.id}) in order to verify your ZEBEDEE account.`);
 		  return;
 		}
 
 		await member.roles.add(role.id);
-		await interaction.editReply('Gamertag verified! Role added.');
+		await interaction.editReply('ZEBEDEE account verified! Role has been added.');
 	},
 };
